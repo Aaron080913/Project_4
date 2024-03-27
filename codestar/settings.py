7151a86cefb0061980a980e6f13609e343a69c6e
Aaron Jones <aaronjjj95@aol.com>
2024-03-27 22:02:53 +0000

    adds a post model

--- codestar/settings.py
+++ codestar/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'SECRET_KEY = os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [ '8000-aaron080913-project4-1z9fw94rjyp.ws-eu110.gitpod.io',
 '.herokuapp.com']
